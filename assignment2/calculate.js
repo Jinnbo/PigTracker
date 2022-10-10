99a5717e02344a6591f1a96cb77c158dc8b762c0
Jimmy <jimmyhui92012@gmail.com>
2022-10-09 23:27:00 -0700

    Finished Stats section

--- assignment2/calculate.js
+++ assignment2/calculate.js
@@ -1,4 +1,6 @@
-
+// TODO:
+/* Check median stat for when n = even # 
+*/
 
 // Selectors for Input Boxes
 var max = document.getElementById('max');
@@ -14,6 +16,11 @@ var cMinus = document.getElementById('c-');
 var d = document.getElementById('d');
 var f = document.getElementById('f');
 
+// Selector for Stats
+var highestStat = document.getElementById('highestStat');
+var lowestStat = document.getElementById('lowestStat');
+var meanStat = document.getElementById('meanStat');
+var medianStat = document.getElementById('medianStat');
 
 // Functions for reading file input
 function init(){
@@ -21,20 +28,108 @@ function init(){
 }
 
 function handFileSelect(event){
-    var reader = new FileReader();
+    const reader = new FileReader();
     reader.onload = handFileLoad;
     reader.readAsText(event.target.files[0]);
 }
 
 function handFileLoad(event){
-    console.log(event.target.result.split(", ")[0]);
+    console.log(event.target.result.split(", ")[0])
+    displayStats(event.target.result,',');
+}
 
-    //document.getElementById('fileContent').textContent = event.target.result;
+function displayStats(str, delimiter = ","){
+
+    // Split string into headers and rows
+    const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+    const rows = str.slice(str.indexOf("\n") + 1).split("\n");   
+
+    // Store student name and percent in dataArray where index 0 is name and index 1 is percent
+    let dataArray = [];
+    let arraySize = rows.length;
+    
+    for (let i = 0;i<rows.length;i++){
+        dataArray.push(rows[i].slice(0,rows[i].indexOf("\r")).split(delimiter));
+    }
+    
+    // Call functions to get highest, lowest, mean, median stats
+    let highestStudent = findHighest(dataArray,arraySize);
+    let lowestStudent = findLowest(dataArray,arraySize);
+    let meanGrade = findMeanGrade(dataArray,arraySize);
+    let medianGrade = findMedianGrade(dataArray,arraySize);
+
+    // Add the stats to the html
+    highestStat.innerHTML = highestStudent;
+    lowestStat.innerHTML = lowestStudent;
+    meanStat.innerHTML = meanGrade;
+    medianStat.innerHTML = medianGrade;
+}   
+
+// Function for finding highest Stat
+function findHighest(dataArray, length){
+    let maxGrade = parseFloat(dataArray[0][1]);
+    let maxStudent;
+
+    for (let i = 1; i < length;i++ ){
+        if (parseFloat(dataArray[i][1]) > parseFloat(maxGrade)){
+            maxGrade = dataArray[i][1];
+            maxStudent = dataArray[i][0];
+        }
+    }
+
+    maxStudent = maxStudent.trim();
+    let maxStat = maxStudent+" ("+maxGrade+"%)"
+    return maxStat;
+}   
+
+// Function for finding lowest Stat
+function findLowest(dataArray, length){
+    let minGrade = parseFloat(dataArray[0][1]);
+    let minStudent;
+
+    for (let i = 1;i < length;i++){
+        if (parseFloat(dataArray[i][1]) < parseFloat(minGrade)){
+            minGrade = dataArray[i][1];
+            minStudent = dataArray[i][0];
+        }
+    }
+
+    minStudent = minStudent.trim();
+    let minStat = minStudent+" ("+minGrade+"%)"
+    return minStat;
 }
 
 
+// Function for finding mean Stat
+function findMeanGrade(dataArray, length){
+    let mean = 0;
 
-// Read and Parse input from .txt file 
+    for (let i = 0;i <length;i++){
+        mean += parseFloat(dataArray[i][1]);
+    }
 
-// Have 4 functions that generate stats
+    return (mean/length).toFixed(2) + "%";
+}
+
+
+// Function for finding median Stat
+function findMedianGrade(dataArray,length){
+
+    let gradeList = [];
+
+    for (let i = 0;i<length;i++){
+        gradeList.push(parseFloat(dataArray[i][1]));
+    }
+
+    gradeList.sort(function(a,b) { return a-b;});
+
+    // If length is even
+    if (length%2==0){
+        return ((gradeList[(parseInt(length)/2)] + gradeList[ (parseInt(length)/2)+1])/2) + "%";
+    }
+    // If length is odd
+    else{
+        return (gradeList[(parseInt(length)+1)/2]) + "%";
+    }
+}
 
