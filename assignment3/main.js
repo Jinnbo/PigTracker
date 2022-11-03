6a3ae8e325fea425446459a4d07b834a440a606d
jimmy <jimmyhui92012@gmail.com>
2022-11-02 20:04:59 -0700

    Added guard for checking empty inputs

--- assignment3/main.js
+++ assignment3/main.js
@@ -1,19 +1,37 @@
 System.register([], function (exports_1, context_1) {
     "use strict";
-    var addTable, name;
+    var addTable, nameBox, heightBox, weightBox, personalityBox, categoryBox, name, height, weight, personality, category;
     var __moduleName = context_1 && context_1.id;
     return {
         setters: [],
         execute: function () {
-            addTable = document.getElementById("addTable");
-            name = document.getElementById("name");
-            name.oninput = () => { console.log(name.textContent); };
-            document.getElementById("addBTN").addEventListener('click', function () {
-                // Open up the add new pig menu to the right of Pig collection table 
-                addTable.style.visibility = "visible";
+            addTable = document.getElementById('addTable');
+            nameBox = document.getElementById('name');
+            heightBox = document.getElementById('height');
+            weightBox = document.getElementById('weight');
+            personalityBox = document.getElementById('personality');
+            categoryBox = document.getElementById('category');
+            category = "grey";
+            // Add Event Listeners to each input box
+            nameBox.onchange = () => { name = nameBox.value; };
+            heightBox.onchange = () => { height = heightBox.value; };
+            weightBox.onchange = () => { weight = weightBox.value; };
+            personalityBox.onchange = () => { personality = personalityBox.value; };
+            categoryBox.onchange = () => { category = categoryBox.value; };
+            document.getElementById('submitBTN').addEventListener('click', function () {
+                // Check if user inputed all boxes
                 // Get all the Info (name, weight, height, category, ...)
                 // Create new pig with pig constructor with all the info above
                 // Add new pig to pig controller
+                console.log(name);
+                console.log(height);
+                console.log(weight);
+                console.log(personality);
+                console.log(category);
+            });
+            document.getElementById("addBTN").addEventListener('click', function () {
+                // Open up the add new pig menu to the right of Pig collection table 
+                addTable.style.visibility = "visible";
             });
         }
     };
--- assignment3/main.ts
+++ assignment3/main.ts
@@ -1,20 +1,44 @@
 
 import {Pig} from './Pigmodel'
 
-var addTable = document.getElementById("addTable")!
-var name = document.getElementById("name")!
+var addTable = document.getElementById('addTable')!
+var nameBox = <HTMLInputElement>document.getElementById('name')!
+var heightBox = <HTMLInputElement>document.getElementById('height')!
+var weightBox = <HTMLInputElement>document.getElementById('weight')!
+var personalityBox = <HTMLInputElement>document.getElementById('personality')!
+var categoryBox = <HTMLInputElement>document.getElementById('category')!
 
-name.oninput = () => {console.log(name.value)}
 
-document.getElementById("addBTN")!.addEventListener('click', function(){
+// Pig attributes
+var name: string;
+var height: string;
+var weight: string;
+var personality: string;
+var category: string = "grey";
 
-    // Open up the add new pig menu to the right of Pig collection table 
-    addTable.style.visibility = "visible";
+// Add Event Listeners to each input box
+nameBox.onchange = () =>{name = nameBox.value}
+heightBox.onchange = () =>{height = heightBox.value}
+weightBox.onchange = () =>{weight = weightBox.value}
+personalityBox.onchange = () =>{personality = personalityBox.value}
+categoryBox.onchange = () =>{category = categoryBox.value}
+
+document.getElementById('submitBTN')!.addEventListener('click',function(){
+
+    // Check if user inputed all boxes
 
     // Get all the Info (name, weight, height, category, ...)
-
     // Create new pig with pig constructor with all the info above
-
     // Add new pig to pig controller
+    console.log(name);
+    console.log(height);
+    console.log(weight);
+    console.log(personality);
+    console.log(category);
+})
+
+document.getElementById("addBTN")!.addEventListener('click', function(){
+    // Open up the add new pig menu to the right of Pig collection table 
+    addTable.style.visibility = "visible";
 })
 
