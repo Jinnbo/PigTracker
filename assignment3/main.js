985403f519cb554a0289c0aabad04e39f6e6fbd5
jimmy <jimmyhui92012@gmail.com>
2022-11-04 00:22:55 -0700

    Fixed add button

--- assignment3/main.js
+++ assignment3/main.js
@@ -1,6 +1,6 @@
 System.register([], function (exports_1, context_1) {
     "use strict";
-    var addTable, nameBox, heightBox, weightBox, personalityBox, categoryBox, name, height, weight, personality, category;
+    var addTable, nameBox, heightBox, weightBox, personalityBox, categoryBox, dynamicBoxOne, dynamicBoxTwo, name, height, weight, personality, category;
     var __moduleName = context_1 && context_1.id;
     return {
         setters: [],
@@ -11,15 +11,38 @@ System.register([], function (exports_1, context_1) {
             weightBox = document.getElementById('weight');
             personalityBox = document.getElementById('personality');
             categoryBox = document.getElementById('category');
-            category = "grey";
+            dynamicBoxOne = document.getElementById('dynamic1');
+            dynamicBoxTwo = document.getElementById('dynamic2');
             // Add Event Listeners to each input box
             nameBox.onchange = () => { name = nameBox.value; };
             heightBox.onchange = () => { height = heightBox.value; };
             weightBox.onchange = () => { weight = weightBox.value; };
             personalityBox.onchange = () => { personality = personalityBox.value; };
-            categoryBox.onchange = () => { category = categoryBox.value; };
+            categoryBox.onchange = () => {
+                category = categoryBox.value;
+                // LOAD Dynamic Boxes once category is choosen 
+                switch (categoryBox.value) {
+                    case "Grey":
+                        console.log("grey");
+                        dynamicBoxOne.innerHTML = "";
+                        break;
+                    case "Chestnut":
+                        console.log("Chestnut");
+                        break;
+                    case "White":
+                        console.log("White");
+                        break;
+                    case "Black":
+                        console.log("Black");
+                        break;
+                }
+            };
             document.getElementById('submitBTN').addEventListener('click', function () {
                 // Check if user inputed all boxes
+                if (name == null || height == null || weight == null || personality == null || category == null) {
+                    console.log("No input");
+                    return;
+                }
                 // Get all the Info (name, weight, height, category, ...)
                 // Create new pig with pig constructor with all the info above
                 // Add new pig to pig controller
--- assignment3/main.ts
+++ assignment3/main.ts
@@ -7,6 +7,9 @@ var heightBox = <HTMLInputElement>document.getElementById('height')!
 var weightBox = <HTMLInputElement>document.getElementById('weight')!
 var personalityBox = <HTMLInputElement>document.getElementById('personality')!
 var categoryBox = <HTMLInputElement>document.getElementById('category')!
+var dynamicBoxOne = <HTMLInputElement>document.getElementById('dynamic1')!
+var dynamicBoxTwo = <HTMLInputElement>document.getElementById('dynamic2')!
+
 
 
 // Pig attributes
@@ -14,18 +17,44 @@ var name: string;
 var height: string;
 var weight: string;
 var personality: string;
-var category: string = "grey";
+var category: string;
 
 // Add Event Listeners to each input box
 nameBox.onchange = () =>{name = nameBox.value}
 heightBox.onchange = () =>{height = heightBox.value}
 weightBox.onchange = () =>{weight = weightBox.value}
 personalityBox.onchange = () =>{personality = personalityBox.value}
-categoryBox.onchange = () =>{category = categoryBox.value}
+
+categoryBox.onchange = () =>{category = categoryBox.value; 
+    // LOAD Dynamic Boxes once category is choosen 
+    switch(categoryBox.value){
+        case "Grey":
+            console.log("grey");
+            dynamicBoxOne.innerHTML = ""
+            break;
+        case "Chestnut":
+            console.log("Chestnut");
+            break;
+        case "White":
+            console.log("White");
+            break;
+        case "Black":
+            console.log("Black");
+            break;
+    }
+}
+
+
+
 
 document.getElementById('submitBTN')!.addEventListener('click',function(){
 
     // Check if user inputed all boxes
+
+    if (name == null || height == null || weight == null || personality == null || category == null){
+        console.log("No input");
+        return;
+    } 
 
     // Get all the Info (name, weight, height, category, ...)
     // Create new pig with pig constructor with all the info above
