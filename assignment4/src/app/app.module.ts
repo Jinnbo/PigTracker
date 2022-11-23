0545ab1576b7a8766c04d41e6bc1a48a1406bfc1
Jimmy <jimmyhui92012@gmail.com>
2022-11-22 19:36:56 -0800

    Added create pig report btn

--- assignment4/src/app/app.module.ts
+++ assignment4/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { PigListComponent } from './pig-list/pig-list.component';
+import { PigAddComponent } from './pig-add/pig-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    PigListComponent
+    PigListComponent,
+    PigAddComponent
   ],
   imports: [
     BrowserModule
---
+++ assignment4/src/app/pig-add/pig-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pig-add',
+  templateUrl: './pig-add.component.html',
+  styleUrls: ['./pig-add.component.css']
+})
+export class PigAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
