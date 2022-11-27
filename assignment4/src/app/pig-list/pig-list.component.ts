215c7aae05f5e0d44d9b05016f7cff563d47afc8
Jimmy <jimmyhui92012@gmail.com>
2022-11-27 00:34:08 -0800

    Added more info modal

--- assignment4/src/app/pig-list/pig-list.component.ts
+++ assignment4/src/app/pig-list/pig-list.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { PigService } from '../pig.service';
 import { Pig } from '../Pig';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pig-list',
@@ -12,16 +13,18 @@ export class PigListComponent implements OnInit {
 
   public pigs;
 
-  constructor(private ps: PigService){
+  constructor(private ps: PigService,private modalService: NgbModal){
     this.pigs = [];    
   }
   
   ngOnInit(): void {
     this.ps.getPigs().subscribe((data:any)=>{
       this.pigs = data
-      //console.log(this.pigs[0].data[0].name)
     })
   }
   
+  open(content, values) {
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result;
+	}
 
 } 
